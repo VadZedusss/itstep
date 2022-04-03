85738e682003f152531aa1b26d065c957a1903e4
Vadym <marimonap93@gmail.com>
2022-04-03 12:14:35 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-class Student:
-    amount_of_student=0
-    def __init__(self, height=160):
-        self.height=height
-        Student.amount_of_student
-    def grow(self, height= 1):
-        self.height+=height
-nick=Student()
-kate=Student(height=170)
-nick.grow(height=15)
-print(kate.height)
-print(nick.height)
+print ("Гуд морненг. Не ставте two, бякос і ем фром Юкраін")
+print ("Окей?")
+print ("Slava Ukraini")
