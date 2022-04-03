1d43fe18fcfe2fed28695c81db7e37897c40ef28
Vadym <marimonap93@gmail.com>
2022-04-03 09:57:24 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("Hello world! I dk who I am")
+class Student:
+    amount_of_student=0
+    def __init__(self, height=160):
+        self.height=height
+        Student.amount_of_student
+    def grow(self, height= 1):
+        self.height+=height
+nick=Student()
+kate=Student(height=170)
+nick.grow(height=15)
+print(kate.height)
+print(nick.height)
